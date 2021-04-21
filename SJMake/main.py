7ed2127bf2cd7558a0d1453fb3cfd1eac2f51ebc
程序圆 <54022108+aqz236@users.noreply.github.com>
2021-04-22 04:47:49 +0800

    Update main.py

--- SJMake/main.py
+++ SJMake/main.py
@@ -4,10 +4,22 @@ T1 = time.time()
 
 #自定义批量生成任务
 #格式为 ('省名称','运营商名称 移动/联通/电信')
-# 如phoneHead.seven.randomSeven('北京','电信')
-phoneHead.seven.randomSeven('广西','联通')
-T2 = time.time()
-print('生成完毕！一共花费:%s秒' % ((T2 - T1)))
+province = '江苏'
+Operators = '联通'
+def main(province,Operators):
+  phoneHead.seven.randomSeven(province,Operators)
+  T2 = time.time()
+  print('生成完毕！一共花费:%s秒' % ((T2 - T1)))
+
+main(province,Operators)
+
+
+
+
+# 如有循环需要可以自行开启循环
+# if __name__ == '__main__':
+#     while True:
+#         main()
 
 
 
